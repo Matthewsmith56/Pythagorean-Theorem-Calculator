44c6d1678b2c450ac971a0e53fb7a47c17c04979
Matthew b Smith <Matthewsmith1030@mail.com>
2018-08-15 10:03:22 -0400

    Add project files.

---
+++ Testing 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("pick a number between 1 and 100");           
+            double xValue = Double.Parse(Console.ReadLine());         
+        }
+    }
+}
